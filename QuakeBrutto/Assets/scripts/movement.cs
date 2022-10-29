5bbf47e18f5de3eff48dc5ea01cf53a5e9f7c8f3
Zerò <lorpat04@gmail.com>
2022-10-29 11:19:53 +0200

    Movement upgrade
    ho modificato un po' i valori delle variabili usate, va aggiustato il salto

--- QuakeBrutto/Assets/scripts/movement.cs
+++ QuakeBrutto/Assets/scripts/movement.cs
@@ -16,7 +16,7 @@ public class movement : MonoBehaviour
     public float jumpHeight = 3f;
 
     Vector3 velocity;
-    bool isGrounded;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +27,12 @@ public class movement : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // per controllare se il player tocca terra
-        
+
         if (isGrounded && velocity.y < 0) // se l'oggetto è a terra, la velocità non deve continuare ad aumentare, quindi viene resettata
         {                                   // si setta a -2f perché è meglio di 0f
             velocity.y = -2f;
         }
-
+        
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
@@ -44,9 +44,9 @@ public class movement : MonoBehaviour
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // formula fisica per determinare la velocità necessaria a compiere un salto di una certa altezza
         }
-
+        
         velocity.y += gravity * Time.deltaTime; // moto uniformemente accelerato
-
+        
         characterController.Move(velocity * Time.deltaTime);
     }
 }
